0ff89c4da4b3576a7e83f79a278155e194c4e098
HSKKOU <hskk1120551@gmail.com>
2017-02-15 14:42:45 +0900

    create scripts from UML

---
+++ Assets/Scripts/GameManager/AGameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Utils.StateMachine;
+
+namespace GameManager
+{
+  public enum GameState
+  {
+    Init,
+    Title,
+    Ready,
+    Playing,
+    Result,
+    Finished,
+    Num
+  }
+
+  public class AGameManager : StatefulSingletonMono<AGameManager, GameState>
+  {
+
+  }
+
+}
